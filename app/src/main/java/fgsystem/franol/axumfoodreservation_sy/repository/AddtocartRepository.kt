5176f1e93eb1e3bac531a94fb4ffd04ef5f87cf5
franol <franolgeleta5@gmail.com>
2019-07-10 09:12:36 +0300

    data binding Fixed

--- app/src/main/java/fgsystem/franol/axumfoodreservation_sy/repository/AddtocartRepository.kt
+++ app/src/main/java/fgsystem/franol/axumfoodreservation_sy/repository/AddtocartRepository.kt
@@ -3,6 +3,13 @@ package fgsystem.franol.axumfoodreservation_sy.repository
 import androidx.lifecycle.LiveData
 import fgsystem.franol.axumfoodreservation_sy.data.Addtocart
 import fgsystem.franol.axumfoodreservation_sy.data.AddtocartDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import java.net.ConnectException
+import java.util.ArrayList
 
 class AddtocartRepository(private val addtocartDao: AddtocartDao){
 
@@ -13,3 +20,5 @@ class AddtocartRepository(private val addtocartDao: AddtocartDao){
         addtocartDao.insertFood(addtocart)
     }
 }
+
+
